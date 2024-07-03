27ea122cdadef8771ea15aa49c0c05ff7020f29c
bhavya jain <bhavyajain4274@gmail.com>
2024-07-03 11:53:34 +0530

    initialized express

--- expressJs/index.js
+++ expressJs/index.js
@@ -1,12 +1,65 @@
 const express = require('express');
 
-const app = express(); let courses = [
-    { id: 1, name: "java" },
-    { id: 2, name: "javascript" },
-    { id: 3, name: "python" }
-]; app.get('/courses', (req, res) => {
-    res.json(courses);
-}); app.listen(3000, () => {
-    console.log("server started")
-});
+const app = express();
 
+app.use(express.json());
+app.use(loggerMiddleware);
+
+let courese = [
+    {id: 1, name: "John", email: "john@example.com"},
+    {id: 2, name: "John", email: "john@example.com"},
+    {id: 3, name: "Johan", email: "john@example.com"}
+]
+
+app.get('/', (req, res) => {
+        res.json(courese);
+    }
+);
+
+app.post('/', (req, res) => {
+    let singleCourse = {
+        id: courese.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    }
+    courese.push(singleCourse);
+    res.json(courese);
+}
+);
+
+app.put('/:id', (req, res) => {
+    let course = courese.find(c => c.id === parseInt(req.params.id));
+
+    course.name = req.body.name;
+    course.email = req.body.email;
+    res.json(course);
+
+}
+);
+
+app.delete('/:id', (req, res) => {
+    let course = courese.find(c => c.id === parseInt(req.params.id));
+    let index = courese.indexOf(course);
+    courese.splice(index, 1);
+    res.json(courese);
+}
+);
+
+function sampleMiddleware(req, res, next) {
+    console.log('This is a sample middleware');
+    next();
+}
+
+// contains method, ip, hostname, date
+function loggerMiddleware(req, res, next) {
+    console.log("method: " + req.method);
+    console.log("ip: " + req.ip);
+    console.log("hostname: " + req.hostname);
+    console.log("date: " + new Date());
+    next();
+}
+
+
+app.listen(3000,
+    () => console.log('Listening on port 3000...')
+)
